c9889c39457ea93594533c2afba7757a0903a023
mitenpatel-lab <miten.patel@mindinventory.com>
2025-12-11 10:18:17 +0530

    update schema

--- server/models/User.js
+++ server/models/User.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema(
     {
+        userName: { type: String, require: true },
         name: { type: String, required: true },
         email: { type: String, required: true, unique: true },
         phone: { type: String },
