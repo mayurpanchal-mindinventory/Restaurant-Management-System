969eab564ec0d08e6f6277b1df3de35936122f86
mitenpatel-lab <miten.patel@mindinventory.com>
2025-12-11 10:33:12 +0530

    update model classes

--- server/models/Restaurant.js
+++ server/models/Restaurant.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 
 const restaurantSchema = new mongoose.Schema(
     {
-        email: { type: String, required: true, unique: true },       // login email
-        passwordHash: { type: String, required: true },              // login password
+        email: { type: String, required: true, unique: true },
+        passwordHash: { type: String, required: true },
 
         name: { type: String, required: true },
         description: { type: String },
