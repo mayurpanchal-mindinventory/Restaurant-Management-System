91256a60eca2626ae055e159070e0247ca2097f1
mayurpanchal-mindinventory <mayur.panchal@mindinventory.com>
2025-12-30 19:03:29 +0530

    12weqwe

--- server/src/utils/generateToken.js
+++ server/src/utils/generateToken.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 
 exports.generateAccessToken = (user) => {
   return jwt.sign({ user }, process.env.ACCESS_TOKEN_SECRET, {
-    expiresIn: "30s",
+    expiresIn: "20m",
   });
 };
 
